0c533460e40c8955a10390da738ab91edd931957
masappe <masato6820@gmail.com>
2019-02-26 10:13:59 +0900

    Initial Commit

---
+++ ToGet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToGet
+//
+//  Created by Masato Hayakawa on 2019/02/26.
+//  Copyright © 2019 masappe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
